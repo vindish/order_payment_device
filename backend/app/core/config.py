c91bbd5e1896c36f7a7ca35fd6d38f14c5742cb3
Vindish <5519296@qq.com>
2026-05-01 16:16:08 +0800

    feat: migrate database from sqlite to postgresql

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,10 +2,17 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     APP_NAME: str = "Device System"
+
     SECRET_KEY: str = "super-secret-key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
 
-    DATABASE_URL: str = "sqlite:///./test.db"
+    # DATABASE_URL: str = "sqlite:///./test.db"
+    # DATABASE_URL =      "postgresql+psycopg2://app:app123@postgres:5432/app_db"W
+    DATABASE_URL: str = "postgresql+psycopg2://app:app123@postgres:5432/app_db"
+
+    # ✅ 新增
+    REDIS_HOST: str = "localhost"
+    MQTT_HOST: str = "localhost"
 
 settings = Settings()
--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if settings.DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        settings.DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
